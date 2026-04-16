c757cc17dc0efd74b6a9b216abe598eacca4365d
AyaNaimi <ayanaimi21@gmail.com>
2026-04-16 22:13:17 +0100

    Remove default string length in AppServiceProvider
    Removed default string length setting for Schema.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\Schema;
 
 use App\Services\PriceEstimationService;
 use App\Services\TranslationService;
@@ -28,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-            Schema::defaultStringLength(191);
-
+//
     }
 }
